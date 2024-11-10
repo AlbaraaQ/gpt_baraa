e32160df95156e18fe70bccda95def1157af4271
AlbaraaQ <111914470+AlbaraaQ@users.noreply.github.com>
2024-11-10 22:48:51 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="gpt_baraa",  # قم بتسمية المشروع
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[
+        # ضع المتطلبات هنا أو اتركها فارغة إذا كانت مذكورة في requirements.txt
+    ],
+)
