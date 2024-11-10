5df114e33b6bbebc423d73d6e6db412f934806b7
AlbaraaQ <111914470+AlbaraaQ@users.noreply.github.com>
2024-11-10 05:12:46 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,7 @@ from huggingface_hub import login
 import videogen_hub
 
 # تسجيل الدخول باستخدام Token الخاص بك
-login(token="5ba5c96173d4c62eab6e81edc5abc3f32c4a8c2aa732ef6edbdb2135d27ffdeb")
-
+login(token="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")
 # تحميل النموذج وتنفيذ الاستدلال
 model = videogen_hub.load('VideoCrafter2')
 video = model.infer_one_video(prompt="A child excitedly swings on a rusty swing set, laughter filling the air.")
