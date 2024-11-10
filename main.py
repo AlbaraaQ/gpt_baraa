ec0e60797a5c138a09e4152e8c908c3f53e36564
AlbaraaQ <111914470+AlbaraaQ@users.noreply.github.com>
2024-11-10 21:53:23 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import videogen_hub
 # تسجيل الدخول باستخدام Token الخاص بك
 login(token="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")
 # تحميل النموذج وتنفيذ الاستدلال
-model = videogen_hub.load('VideoCrafter2')
+model = videogen_hub.load('LaVie')
 video = model.infer_one_video(prompt="A child excitedly swings on a rusty swing set, laughter filling the air.")
 
 # هنا الفيديو هو tensor من نوع torch وحجمه torch.Size([16, 3, 320, 512])
